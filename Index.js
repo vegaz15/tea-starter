2caecc94e20e71c64dfd4c6179fd39e937bb635d
vegaz15 <47427736+vegaz15@users.noreply.github.com>
2024-05-23 02:20:10 +0100

    Index.js

---
+++ Index.js
@@ -0,0 +1,14 @@
+// index.js
+
+/**
+ * Adds two numbers.
+ * @param {number} a The first number.
+ * @param {number} b The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+function add(a, b) {
+  return a + b;
+}
+
+// Export the add function so it can be used by other modules.
+module.exports = add;
