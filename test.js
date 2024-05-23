d64fa5e7bee63ec49902fee676690b9618a9eb04
vegaz15 <47427736+vegaz15@users.noreply.github.com>
2024-05-23 02:22:40 +0100

    test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+// test.js
+
+// Import the add function from our npm package.
+const add = require('./index');
+
+// Test the add function with some sample inputs.
+console.log(add(5, 3)); // Output should be 8
+console.log(add(-2, 10)); // Output should be 8
